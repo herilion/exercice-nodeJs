6dbc70a8a89cedb8a37ebc8ca742fc8346530fe5
herilion <heritierlionge@yahoo.com>
2022-10-13 14:33:06 +0200

    les evenements

--- script.js
+++ script.js
@@ -14,9 +14,15 @@ const monEvenement = new events.EventEmitter();
 monEvenement.addListener('se_connecter', () => {
     console.log('un utlisateur vient de se connecter');
 })
+monEvenement.addListener('timeout', () => {
+    console.log('2 secondes se sont écoulées');
+})
 const server = http.createServer();
 server.addListener('request', (req, res) => {
     monEvenement.emit('se_connecter');
     res.end('<h1>Bienvenu dans notre site</h1>');
 })
+setInterval(() => {
+    monEvenement.emit('timeout');
+}, 2000)
 server.listen(8000);
