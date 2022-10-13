6b65b4314ddbb35ae75b932ad7787fd952e01954
herilion <heritierlionge@yahoo.com>
2022-10-13 14:30:25 +0200

    utilisation de emit

--- script.js
+++ script.js
@@ -9,8 +9,14 @@
 // }, 1000)
 
 const http = require('http');
+const events = require('events');
+const monEvenement = new events.EventEmitter();
+monEvenement.addListener('se_connecter', () => {
+    console.log('un utlisateur vient de se connecter');
+})
 const server = http.createServer();
 server.addListener('request', (req, res) => {
+    monEvenement.emit('se_connecter');
     res.end('<h1>Bienvenu dans notre site</h1>');
 })
 server.listen(8000);
