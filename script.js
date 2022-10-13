39ea7c01456131876755099a451ab70b7bfb7ca5
herilion <heritierlionge@yahoo.com>
2022-10-13 14:25:20 +0200

    les evenements

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-//utilisation de Timer
-const monErreur = new Error('une erreur est detectée');
-setTimeout(() => {
-    throw monErreur
-}, 1000)
+// //utilisation de Timer
+// const monErreur = new Error('une erreur est detectée');
+// setTimeout(() => {
+//     try {
+//         throw monErreur
+//     } catch (error) {
+
+//     }
+// }, 1000)
+
+const http = require('http');
+const server = http.createServer();
+server.addListener('request', (req, res) => {
+    res.end('<h1>Bienvenu dans notre site</h1>');
+})
+server.listen(8000);
