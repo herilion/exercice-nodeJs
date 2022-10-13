765e3ec461118bd8658e7e128eb56ec412f38fa4
herilion <heritierlionge@yahoo.com>
2022-10-13 09:45:17 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const fs = require('fs')
+const server = http.createServer();
+server.on('request', (req, res) => {
+    let datahtml = ''
+    fs.readFile('./index.html', 'utf-8', (err, data) => {
+        if (err) throw err;
+        res.writeHead(200, {
+            'content-Type': 'text/html;charset=utf - 8',
+        });
+        res.write(data);
+        return res.end();
+    })
+})
+server.listen(9000);
